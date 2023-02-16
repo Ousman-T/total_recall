7c74d1a90ecb3234e62939f068be2b9fd8b51bbd
Ousman Touray <otouray14@gmail.com>
2023-02-16 10:45:41 -0500

    instantiated class, still fixing functions to eat, NOT DONE

--- day_three.js
+++ day_three.js
@@ -1,58 +1,74 @@
 class Hamster {
-    constructor(name){
-      this.owner = ''
-      this.name = name
-      this.price = 15
+    constructor(name) {
+        this.owner = ''
+        this.name = name
+        this.price = 15
     }
-    wheelRun(){
-      console.log('squeak squeak')
+    wheelRun() {
+        console.log('squeak squeak')
     }
-    eatFood(){
-      console.log('nibble nibble')
+    eatFood() {
+        console.log('nibble nibble')
     }
-    getPrice(){
-      return this.price
+    getPrice() {
+        return this.price
     }
-  }
-  class Person {
-    constructor(name){
-      this.name = name
-      this.age = 0
-      this.height = 0
-      this.weight = 0
-      this.mood = 0
-      this.hamsters = []
-      this.bankAccount = 0
+}
+class Person {
+    constructor(name) {
+        this.name = name
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
     }
-    getName(){
-      return this.name
+    getName() {
+        return this.name
     }
-    getAge(){
-      return this.age
+    getAge() {
+        return this.age
     }
-    getWeight(){
-      return this.weight
+    getWeight() {
+        return this.weight
     }
-    greet(){
-      console.log(`I am ${this.name} hello`)
+    greet() {
+        console.log(`I am ${this.name} hello`)
     }
-    eat(){
-      this.weight++
-      this.mood++
+    eat() {
+        this.weight++
+        this.mood++
     }
-    exercise(){
-      this.weight--
+    exercise() {
+        this.weight--
     }
-    ageUp(){
-      this.age++
-      this.height++
-      this.weight++
-      this.mood--
-      this.bankAccount+=10
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
     }
-    buyHamster(hamster){
-      this.hamsters.push(hamster)
-      this.mood+=10
-      this.bankAccount-=hamster.getPrice()
+    buyHamster(hamster) {
+        this.hamsters.push(hamster)
+        this.mood += 10
+        this.bankAccount -= hamster.getPrice()
     }
-  }
+}
+
+const timmy = new Person("Timmy");
+const repeatFun = () => {
+    for(let i = 0; i < 5; i++){
+        timmy.ageUp();
+    }
+};
+repeatFun();
+console.log(timmy);
+
+const eatUp = () => {
+    for(let j = 0; j < 5; j++)
+    timmy.eat()
+};
+eatUp();
+console.log(timmy);
